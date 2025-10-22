c9f9324e0fc36015029550825da57ba0b5d87131
PedroHenriquealmeida1405 <suckeldre@gmail.com>
2025-10-21 21:40:17 -0300

    Add files via upload

---
+++ src/util/Mensagem.java
@@ -0,0 +1,14 @@
+package util;
+
+import javax.swing.JOptionPane;
+
+public class Mensagem {
+    public static void info(String msg) {
+        JOptionPane.showMessageDialog(null, msg, "Informação",
+        JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void erro(String msg) {
+        JOptionPane.showMessageDialog(null, msg, "Erro", JOptionPane.ERROR_MESSAGE);
+    }
+}
