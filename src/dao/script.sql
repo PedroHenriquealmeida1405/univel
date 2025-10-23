32dca4fa1488d5a6c47708b4029ae11ac35fc898
PedroHenriquealmeida1405 <suckeldre@gmail.com>
2025-10-23 20:24:48 -0300

    Add usuarios table with initial data insertion

--- src/dao/script.sql
+++ src/dao/script.sql
@@ -1,11 +1,16 @@
 CREATE DATABASE univel4a;
-
 USE univel4a;
+
 
 CREATE TABLE usuarios (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nome_usuario VARCHAR(50) NOT NULL,
-    data_criacao TIMESTAMP
+    senha_hash VARCHAR(64) NOT NULL,
+    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    tipo VARCHAR(1) CHECK (tipo IN('U', 'A')) DEFAULT 'U',
+    ativo VARCHAR(1) CHECK (ativo IN('T', 'F')) DEFAULT 'T'
     );
+    
+INSERT INTO usuarios VALUES(1, 'pedro ', '123456789',current_date(),'A','T');
     
 
