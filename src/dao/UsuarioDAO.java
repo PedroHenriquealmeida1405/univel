2f4f8f784e6a8c811ac88056f7a97603172666de
PedroHenriquealmeida1405 <suckeldre@gmail.com>
2025-10-28 21:12:08 -0300

    Update UsuarioDAO.java

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -2,8 +2,13 @@ package dao;
 
 import model.Usuario;
 import util.Funcoes;
+import util.ResultadoCadastro
+
+
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
 
@@ -71,7 +76,79 @@ public class UsuarioDAO {
                 PreparedStatement stmt = conn.prepareStatement(sql)) {
                     
             stmt.setString(2, usuario.getLogin());
-                }
+            stmt.setString(2, Funcoes.gerarHashSHA256(getSenha()));
+            stmt.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+            stmt.setString(4, "U");
+            stmt.setString(5, "T");
+            stmt.executeUpdate();
+
+            return ResultadoCadastro.SUCESSO;
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir usuario: " + e.getMessage());
+            return ResultadoCadastro.ERRO_BANCO;
+        }
+    } 
+
+    public List<Usuario> listarTodos() {
+        List<Usuario> lista = new ArrayList<>();
+        String sql = "SELECT * FROM usuarios ORDER BY id";
+        try (Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Usuario u = new Usuario();
+                u.setId(rs.getInt("id"));
+                u.setTipo("tipo");
+                u.setAtivo(rs.getString("ativo"));
+                u.setDataCriacao(rs.getString("data_criacao"));
+                lista.add(u);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+    public boolean excluir(int id) {
+        String sql = "DELETE FROM usuarios WHERE id = ?";
+        try (Connection conn = Conexao.conectar();
+            PrepareStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setInt(1, );
+                int r = stmt.executeUpdate();
+                return r > 0;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+    }
+    public Usuario buscarPorId(int id) {
+        String sql = "SELECT * FROM usuarios WHERE id = ?";
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
     
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+    
+            if (rs.next()) {
+                Usuario u = new Usuario();
+                u.setId(rs.getInt("id"));
+                u.setLogin(rs.getString("nome_usuario"));
+                u.setSenha(rs.getString("senha_hash"));
+                u.setTipo(rs.getString("tipo"));
+                u.setAtivo(rs.getString("ativo"));
+                u.setDataCriacao(rs.getString("data_criacao"));
+                return u;
+            }
+    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
 
