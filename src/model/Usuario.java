2af165b09032c1335fe7f88e34d16fc83f209498
PedroHenriquealmeida1405 <suckeldre@gmail.com>
2025-11-04 20:01:16 -0300

    Add files via upload

---
+++ src/model/Usuario.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Usuario { 
+
+    private int id;
+    private String login;
+    private String senha;
+    private String dataCriacao;
+    private String ativo;
+    private String tipo;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public String getDataCriacao() { return dataCriacao; }
+    public void setDataCriacao(String dataCriacao) { this.dataCriacao = dataCriacao; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public String getAtivo() { return ativo; }
+    public void setAtivo(String ativo) { this.ativo = ativo; }
+}
